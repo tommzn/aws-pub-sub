7f9a4605309b3ce27b2b168e278c9ddbe471b5e2
Thomas Schenker <tommzn@gmx.de>
2023-10-30 00:06:43 +0100

    add message types

--- types.go
+++ types.go
@@ -1,3 +1,23 @@
 package pubsub
 
+// MESSAGE_TYPE_ATTRIBUTE is the message attribute name where message type is placed.
+const MESSAGE_TYPE_ATTRIBUTE string = "MESSAGE_TYPE"
+
+const (
+
+	// MESSAGE_TYPE_SNS is used if a message is send via AWS SNS directly.
+	MESSAGE_TYPE_SNS string = "SNS"
+
+	// MESSAGE_TYPE_S3 is used if a message is to large for SNS and therefore be uploaded to ans S4 bucket.
+	MESSAGE_TYPE_S3 string = "S3"
+)
+
+// ObjectId is used to identify messages passed to a persistence layer.
 type ObjectId string
+
+// SnsPublisher is a client to send messages to AWS SNS. In case a message exceeds SNS message size imit a persitence layer
+// can be used to store such a large message and distribute its id via SNS, only.
+type SnsPublisher struct {
+	snsClient   snsClient
+	persistence Persistence
+}
